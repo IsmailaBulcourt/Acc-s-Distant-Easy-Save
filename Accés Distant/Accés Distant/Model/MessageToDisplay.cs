fe613b7cb460da701c233e43fa521f992cd96536
darksasukedu59 <44493087+darksasukedu59@users.noreply.github.com>
2020-12-15 17:16:19 +0100

    creation MessageToDiplay

---
+++ Accés Distant/Accés Distant/Model/MessageToDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accés_Distant.Model
+{
+    class MessageToDisplay
+    {
+        string Name { get; set; }
+        int Progression { get; set; }
+        string TypeSave { get; set; }
+
+    }
+}
