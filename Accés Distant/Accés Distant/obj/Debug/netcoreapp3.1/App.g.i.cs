02b727d628f4acecaaedf3dfa88757599b57c0a3
darksasukedu59 <44493087+darksasukedu59@users.noreply.github.com>
2020-12-16 14:37:11 +0100

    fix

--- Accés Distant/Accés Distant/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Accés Distant/Accés Distant/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Accés_Distant {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Accés_Distant {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public static void Main() {
             Accés_Distant.App app = new Accés_Distant.App();
             app.InitializeComponent();
